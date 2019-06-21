13133f0bdaab5a82be85d3a16446e3b1c6297c03
Ronnie Nyaga <ronnienyaga@gmail.com>
2019-06-21 12:43:06 +0300

    Moving from useState to useReducer

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "ab775de9bbe6157e1664";
+/******/ 	var hotCurrentHash = "b10e51a71405f8cc3a3f";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
