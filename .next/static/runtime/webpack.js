53999a24f5a6c5d9a615d016bf3399fd378d64e3
Ronnie Nyaga <ronnienyaga@gmail.com>
2019-06-21 13:52:14 +0300

    Refactor speakers reducer to its own file

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "ed293f8f8fa3c5864b1b";
+/******/ 	var hotCurrentHash = "99e42ded55def83fdd88";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- src/Speakers.js
+++ src/Speakers.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useContext, useReducer } from 'react';
+import speakersReducer from './speakersReducer';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../static/site.css';
@@ -11,33 +12,6 @@ import { ConfigContext } from  './App';
 const Speakers = ({}) => {
   const [speakingSaturday, setSpeakingSaturday] = useState(true);
   const [speakingSunday, setSpeakingSunday] = useState(true);
-
-  const speakersReducer = (state, action) => {
-    const updateFavorite = (favoriteValue) => {
-      return state.map((item, index) => {
-        if (item.id === action.sessionId) {
-          item.favorite = favoriteValue;
-          return item;
-        }
-        return item;
-      });
-    };
-
-    switch (action.type) {
-      case 'setSpeakerList': {
-        return action.data;
-      }
-      case 'favorite': {
-        return updateFavorite(true);
-      }
-      case 'unfavorite': {
-        return updateFavorite(false);
-      }
-      default: {
-        return state;
-      }
-    }
-  };
 
   // const [speakerList, setSpeakerList] = useState([]);
   const [speakerList, dispatch] = useReducer(speakersReducer, []);
